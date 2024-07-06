de327e37a774bd6180842800be364ecb6e2d07a5
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2024-07-06 11:59:52 -0400

    Created basic arithmetic functions

---
+++ js/main.js
@@ -0,0 +1,23 @@
+let num1;
+let operator;
+let num2;
+
+//adding function
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+//subtracting function
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+//multiplying function
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+//dividing function
+function divide(num1, num2) {
+  return num1 / num2;
+}
