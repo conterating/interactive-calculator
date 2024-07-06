889cc4af9c841006aaddb4726eba858083435d0a
Aashay Gupte <151937841+conterating@users.noreply.github.com>
2024-07-06 14:58:41 -0400

    Implemented html markup for calculator

--- js/main.js
+++ js/main.js
@@ -21,3 +21,15 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate() {
+  if (operator === "+") {
+    add(num1, num2);
+  } else if (operator === "-") {
+    subtract(num1, num2);
+  } else if (operator === "*") {
+    multiply(num1, num2);
+  } else if (operator === "/") {
+    divide(num1, num2);
+  }
+}
